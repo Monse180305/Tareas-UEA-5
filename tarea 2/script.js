0b71dd2701cfc3f494d93be82af85b00ebe237e1
Monse180305 <2233073091@correo.ler.uam.mx>
2025-03-04 21:11:54 -0600

    tarea 2

---
+++ tarea 2/script.js
@@ -0,0 +1,11 @@
+//aquí escribes lo de tus parráfos
+let p1="La tecnología juega un papel importante y prometedor, en especial para esta carrera de Arte y Comunicación Digital, ya que nosotros, como artistas en formación, tomamos los nuevos recursos que nos ofrece el presente para dar a conocer nuestro trabajo con la misma intención de importancia que tiene el arte plástico tradicional. Hablando específicamente de tecnologías web, como HTML, CSS y JavaScript, resultan ventajosas, ya que nos permiten crear desde en un espacio como el ciberespacio, el cual está repleto de millones de usuarios interconectados desde varias partes del mundo, haciendo que nuestras obras o trabajos puedan llegar a más personas con facilidad.";
+
+let p2="En lo personal, aunque el desarrollo de estas tecnologías, que implican programación, puede ser un tanto difícil para mí disfrutarlo y aprovecharlo completamente, en el caso de HTML, es relativamente más fácil de manejar, y creo que, con buenas bases de conocimiento, se puede sacar provecho de esta tecnología. Me he demostrado a mí misma que es posible realizar trabajos que, tal vez hace años eran inimaginables para mí, como el construir desde 0 una galería virtual en 3D. Estas tecnologías brindan muchas oportunidades en el ámbito artístico, desde crear una página personal para compartir tu trayectoria profesional hasta desarrollar proyectos concretos como contenidos audiovisuales o encargos personales que contribuyan a tu vida profesional. Su potencial son las diferentes posibilidades que tenemos de innovar la forma en la que presentamos nuestras creaciones en entornos digitales.";
+
+document.getElementById('parr1').innerHTML=p1;
+
+document.getElementById('parr2').innerHTML=p2;
+
+console.log(p1.length);
+console.log(p2.length);
